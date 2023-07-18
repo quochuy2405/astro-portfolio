20caa395c9e699bc0212e767ded0fc140ff82024
quochuy2405 <huy01682312920@gmail.com>
2023-07-19 00:09:11 +0700

    update

--- src/components/moleculers/Header.tsx
+++ src/components/moleculers/Header.tsx
@@ -1,23 +1,129 @@
 import { path } from 'animation'
+import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
+import { BsFacebook, BsGithub, BsLinkedin } from 'react-icons/bs'
+const textScript = [
+  "I'M A SYSTEM INFORMATION ENGINEER",
+  'FRONT-END DEVELOPER',
+  'FULLSTACK DEVELOPER',
+  'ALWAYS CHALLENGE MY SELF'
+]
+const Header = () => {
+  const [text, setText] = useState({
+    index: 0,
+    value: textScript[0]
+  })
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.1
+      }
+    }
+  }
+  const animationItem = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  }
+  useEffect(() => {
+    const time = setInterval(
+      () => {
+        setText((curr) => {
+          let nextIndex = curr.index + 1
+          if (nextIndex > textScript.length - 1) {
+            nextIndex = 0
+          }
+          return { index: nextIndex, value: textScript[nextIndex] }
+        })
+      },
+      (text.value.length / 7) * 1000
+    )
+    return () => clearInterval(time)
+  }, [])
 
-const Header = () => (
-  <div className="container">
-    <motion.svg className="item" viewBox="0 0 456 430" fill="tranparent" xmlns="http://www.w3.org/2000/svg">
-      <motion.path
-        d="M3.21021 276.186C42.0279 284.149 58.735 253.321 66.055 219.437C72.1342 191.296 73.2012 163.582 73.2012 134.943C73.2012 125.682 74.6586 98.413 71.73 107.199C68.3553 117.323 71.3096 132.696 71.3096 143.14C71.3096 163.97 72.498 184.32 75.9336 204.934C81.2505 236.836 87.3431 268.188 91.0668 300.357C94.8021 332.626 100.838 367.004 94.6399 399.354C93.1393 407.186 89.4569 424.368 79.2966 426.677C70.6342 428.646 63.2924 383.263 62.4819 378.545C57.2378 348.025 58.9266 314.122 68.367 284.594C76.9364 257.789 92.4436 233.39 103.678 207.666C112.665 187.089 122.53 166.358 127.639 144.401C130.938 130.221 132.341 115.225 137.517 101.524C139.6 96.0108 136.147 108.124 136.046 109.09C134.164 127.072 136.909 145.296 141.301 162.687C146.247 182.273 151.487 201.622 157.485 220.908C159.943 228.812 162.494 248.582 167.784 230.997C173.931 210.562 178.112 189.655 183.337 168.993C183.939 166.612 188.592 145.022 188.592 158.484C188.592 168.152 188.592 177.82 188.592 187.489C188.592 197.142 189.943 201.222 193.006 188.75C194.774 181.552 196.324 152.516 203.305 149.025C206.315 147.52 203.725 155.748 203.725 159.114C203.725 164.577 203.666 170.045 203.725 175.508C203.901 191.68 209.883 171.129 211.712 166.26C212.76 163.471 222.303 133.479 221.17 148.395C221.087 149.488 221.61 163.184 222.221 162.267C225.82 156.869 222.539 139.961 221.801 134.102C220.335 122.466 215.764 109.085 224.323 99.0017C243.471 76.4436 280.562 65.8882 306.505 56.3345C344.344 42.3996 382.856 30.4251 420.844 16.9251C428.175 14.3199 445.645 9.6927 452.792 3.15809C453.737 2.294 436.186 6.90012 434.506 7.57194"
-        stroke="tranparent"
-        stroke-width="6"
-        stroke-linecap="round"
-        variants={path}
-        initial="hidden"
-        animate="visible"
-        transition={{
-          default: { duration: 1, ease: 'easeInOut' },
-          fill: { duration: 1, ease: [1, 0, 0.8, 1] }
-        }}
-      />
-    </motion.svg>
-  </div>
-)
+  return (
+    <>
+      <header className="p-4 relative z-10">
+        <div className="lg:w-4/5 m-auto flex flex-col md:flex-row">
+          <ul className="flex gap-2 flex-1">
+            <a
+              className="px-3 py-[2px] rounded-md hover:bg-[#e2e2e28f] text-base flex items-center ease-linear duration-150 transition-all h-fit"
+              href="/"
+            >
+              Home
+            </a>
+            <a
+              className="px-3 py-[2px] rounded-md hover:bg-[#e2e2e28f] text-base flex items-center ease-linear duration-150 transition-all h-fit"
+              href="/#about"
+            >
+              About
+            </a>
+            <a
+              className="px-3 py-[2px] rounded-md hover:bg-[#e2e2e28f] text-base flex items-center ease-linear duration-150 transition-all h-fit"
+              href="/#project"
+            >
+              Project
+            </a>
+            <a
+              className="px-3 py-[2px] rounded-md hover:bg-[#e2e2e28f] text-base flex items-center ease-linear duration-150 transition-all h-fit"
+              href="/#career"
+            >
+              Career
+            </a>
+          </ul>
+          <div className="flex-1 flex items-center justify-center">
+            <motion.h1
+              variants={container}
+              key={text.index}
+              initial="hidden"
+              animate="visible"
+              className="font-extrabold uppercase text-transparent text-2xl py-4 bg-clip-text bg-gradient-to-r from-blue-400 to-pink-600"
+            >
+              {text.value.split('').map((txt, index) => (
+                <motion.span key={index} variants={animationItem}>
+                  {txt}
+                </motion.span>
+              ))}
+            </motion.h1>
+          </div>
+
+          <ul className="flex gap-4 flex-1 justify-end">
+            <a href="/">
+              <BsFacebook />
+            </a>
+            <a href="/about">
+              <BsGithub />
+            </a>
+            <a href="/project">
+              <BsLinkedin />
+            </a>
+          </ul>
+        </div>
+      </header>
+      <div className="w-screen h-screen fixed top-0 right-0 left-0 scale-[100%] z-0 flex align-top justify-center opacity-30">
+        <motion.svg viewBox="0 0 456 430" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <motion.path
+            d="M3.21021 276.186C42.0279 284.149 58.735 253.321 66.055 219.437C72.1342 191.296 73.2012 163.582 73.2012 134.943C73.2012 125.682 74.6586 98.413 71.73 107.199C68.3553 117.323 71.3096 132.696 71.3096 143.14C71.3096 163.97 72.498 184.32 75.9336 204.934C81.2505 236.836 87.3431 268.188 91.0668 300.357C94.8021 332.626 100.838 367.004 94.6399 399.354C93.1393 407.186 89.4569 424.368 79.2966 426.677C70.6342 428.646 63.2924 383.263 62.4819 378.545C57.2378 348.025 58.9266 314.122 68.367 284.594C76.9364 257.789 92.4436 233.39 103.678 207.666C112.665 187.089 122.53 166.358 127.639 144.401C130.938 130.221 132.341 115.225 137.517 101.524C139.6 96.0108 136.147 108.124 136.046 109.09C134.164 127.072 136.909 145.296 141.301 162.687C146.247 182.273 151.487 201.622 157.485 220.908C159.943 228.812 162.494 248.582 167.784 230.997C173.931 210.562 178.112 189.655 183.337 168.993C183.939 166.612 188.592 145.022 188.592 158.484C188.592 168.152 188.592 177.82 188.592 187.489C188.592 197.142 189.943 201.222 193.006 188.75C194.774 181.552 196.324 152.516 203.305 149.025C206.315 147.52 203.725 155.748 203.725 159.114C203.725 164.577 203.666 170.045 203.725 175.508C203.901 191.68 209.883 171.129 211.712 166.26C212.76 163.471 222.303 133.479 221.17 148.395C221.087 149.488 221.61 163.184 222.221 162.267C225.82 156.869 222.539 139.961 221.801 134.102C220.335 122.466 215.764 109.085 224.323 99.0017C243.471 76.4436 280.562 65.8882 306.505 56.3345C344.344 42.3996 382.856 30.4251 420.844 16.9251C428.175 14.3199 445.645 9.6927 452.792 3.15809C453.737 2.294 436.186 6.90012 434.506 7.57194"
+            stroke="black"
+            stroke-width="6"
+            stroke-linecap="round"
+            variants={path}
+            initial="hidden"
+            animate="visible"
+            transition={{
+              default: { duration: 2, ease: 'circOut' },
+              fill: { duration: 2, ease: [1, 0, 0.8, 1] }
+            }}
+          />
+        </motion.svg>
+      </div>
+    </>
+  )
+}
 export default Header
--- src/components/organisms/Project.tsx
+++ src/components/organisms/Project.tsx
@@ -2,13 +2,13 @@ import clsx from 'clsx'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useState } from 'react'
 import { GrFormClose } from 'react-icons/gr'
-import { Image } from 'astro:assets'
 interface IProject {
   id: string
   company: string
   title: string
   position: string
   thumbnail: string
+  bg?: string
 }
 const items: IProject[] = [
   {
@@ -16,57 +16,138 @@ const items: IProject[] = [
     company: '(Otoman x AB InBev)',
     title: 'CATABINBEV',
     position: 'Frontend & Machine Learning',
+    thumbnail: '/images/survey.png',
+    bg: 'linear-gradient(160deg,_#0093E9_0%,_#80D0C7_100%)'
+  },
+  {
+    id: '2',
+    company: 'Ftisu teams',
+    title: 'PREDICT CRYPTO PROJECT',
+    position: 'Frontend & Machine Learning',
+    thumbnail: '/images/survey.png'
+  },
+  {
+    id: '4',
+    company: 'Hopee - Software Company',
+    title: 'HOMIS PROJECT',
+    position: 'Frontend',
+    thumbnail: '/images/survey.png'
+  },
+  {
+    id: '5',
+    company: 'Hopee - Software Company',
+    title: 'ASTRAZENECA PROJECT',
+    position: 'Frontend',
+    thumbnail: '/images/survey.png'
+  },
+  {
+    id: '6',
+    company: 'Hopee - Software Company',
+    title: 'KIDS CONNECT PROJECT',
+    position: 'Frontend',
+    thumbnail: '/images/survey.png'
+  },
+  {
+    id: '7',
+    company: 'Beliy Clothings',
+    title: 'BELIY STORE',
+    position: 'FullStack',
     thumbnail: '/images/survey.png'
   }
 ]
+
 const Project = () => {
   const [itemSelected, setItemSelected] = useState<IProject | null>(null)
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  }
+  const animationItem = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  }
+
   return (
-    <div className="grid grid-cols-3 grid-rows-none gap-4 h-[400px] w-2/3">
-      {items.map((item, index) => (
-        <motion.div
-          className={clsx('min-h-[100px] max-h-[300px] bg-white rounded-lg col-span-1 p-3 font-semibold', {
-            '!col-span-2': index % 4 == 1 || index % 4 == 2
-          })}
-          layoutId={item.id}
-          onClick={() => setItemSelected(item)}
-        >
-          <Image
-            width={1000}
-            height={2000}
-            src="https://www.rappler.com/tachyon/2022/04/astro.png"
-            alt="A base64 encoded image"
-          />
-          <motion.h2>{item?.title}</motion.h2>
-          <motion.h5>{item?.company}</motion.h5>
-          <motion.h5>Position: {item?.position}</motion.h5>
-        </motion.div>
-      ))}
-      <AnimatePresence>
-        {itemSelected && (
-          <motion.div
-            initial={{ opacity: 0, scale: 0.75 }}
-            animate={{ opacity: 1, scale: 1 }}
-            exit={{ opacity: 0 }}
-            className="fixed w-screen l-0 top-0 right-0 h-screen flex items-center justify-center bg-[#bbbbbb71]"
-          >
+    <section>
+      <motion.h1
+        variants={container}
+        layoutScroll
+        initial="hidden"
+        animate="visible"
+        className="font-extrabold uppercase text-transparent text-4xl py-4 bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+      >
+        {'PROJECT'.split('').map((text, index) => (
+          <motion.span key={index} variants={animationItem}>
+            {text}
+          </motion.span>
+        ))}
+      </motion.h1>
+      <motion.div
+        variants={container}
+        layoutScroll
+        initial="hidden"
+        animate="visible"
+        className="grid grid-cols-2 md:grid-cols-3 grid-rows-none gap-4 h-full w-full overflow-y-auto"
+      >
+        {items.map((item, index) => {
+          return (
             <motion.div
-              layoutId={itemSelected.id}
-              className="w-1/2 h-1/2 bg-white rounded-lg p-3 relative font-semibold"
-              exit={{ opacity: 0, scale: 0 }}
+              className={clsx(
+                'min-h-[170px] max-h-[200px] gap-1 backdrop-blur-sm cursor-pointer flex flex-col border p-2 rounded-lg text-black hover:border-gray-800 hover:border-2 transition-colors'
+              )}
+              key={index}
+              layoutId={item.id}
+              variants={animationItem}
+              onClick={() => setItemSelected(item)}
             >
-              <div className="flex items-center justify-end absolute top-0 right-0 ">
-                <motion.button onClick={() => setItemSelected(null)}>
-                  <GrFormClose size={28} />
-                </motion.button>
-              </div>
-              <motion.h2>{itemSelected?.title}</motion.h2>
-              <motion.h5>{itemSelected?.company}</motion.h5>
+              <span className="absolute">
+                <motion.h2 className="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                  {item?.title}
+                </motion.h2>
+                <motion.h5>{item?.company}</motion.h5>
+                <motion.h5>Position: {item?.position}</motion.h5>
+              </span>
             </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+          )
+        })}
+        <AnimatePresence>
+          {itemSelected && (
+            <motion.div
+              initial={{ opacity: 0, scale: 0.75 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0 }}
+              className="fixed w-screen left-0 z-20 top-0 right-0 h-screen flex items-center justify-center bg-[#00000075]"
+            >
+              <motion.div
+                layoutId={itemSelected.id}
+                className="w-1/2 h-1/2 bg-white rounded-lg p-3 relative font-semibold"
+                exit={{ opacity: 0, scale: 0 }}
+              >
+                <div className="flex items-center justify-end absolute top-0 right-0 ">
+                  <motion.button onClick={() => setItemSelected(null)}>
+                    <GrFormClose size={28} />
+                  </motion.button>
+                </div>
+                <motion.h2 className="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                  {itemSelected?.title}
+                </motion.h2>
+                <motion.h5>{itemSelected?.company}</motion.h5>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </motion.div>
+    </section>
   )
 }
 
