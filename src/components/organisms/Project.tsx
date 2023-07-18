8a1e7f23b31714610f6b7a2eafb965edeebe1fa8
quochuy2405 <huy01682312920@gmail.com>
2023-07-19 00:22:12 +0700

    up

--- src/components/organisms/Project.tsx
+++ src/components/organisms/Project.tsx
@@ -1,7 +1,6 @@
 import clsx from 'clsx'
 import { AnimatePresence, motion } from 'framer-motion'
 import { useState } from 'react'
-import { GrFormClose } from 'react-icons/gr'
 interface IProject {
   id: string
   company: string
@@ -133,9 +132,12 @@ const Project = () => {
                 className="w-1/2 h-1/2 bg-white rounded-lg p-3 relative font-semibold"
                 exit={{ opacity: 0, scale: 0 }}
               >
-                <div className="flex items-center justify-end absolute top-0 right-0 ">
+                <div className="flex items-center justify-end absolute top-2 right-2 ">
                   <motion.button onClick={() => setItemSelected(null)}>
-                    <GrFormClose size={28} />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
+                      <path d="M11 0.7H13V23.3H11z" transform="rotate(-45.001 12 12)" />
+                      <path d="M0.7 11H23.3V13H0.7z" transform="rotate(-45.001 12 12)" />
+                    </svg>
                   </motion.button>
                 </div>
                 <motion.h2 className="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
